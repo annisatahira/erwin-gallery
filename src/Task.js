5735dd41485d5bd714b246a3aae46e45901d6d4e
Annisa Tahira <annisatahira.jsc@gmail.com>
2023-06-20 10:52:10 +0700

    add project

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "incredible-axolotl-374eb6.netlify.app" // TODO: replace this
+const studentName = "Erwin Renanda Saputra" // TODO: replace this
+const studentId = "FE5161534" // TODO: replace this
+
+export { url, studentName, studentId }
